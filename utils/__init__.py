91fa9743613a16f99bfefc0c4fab1577c37e396e
LahElr <136647680@qq.com>
2021-04-15 15:52:41 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from .dataset import load_from_pickle, save_as_pickle
 from .dataset import FileNxDataset, SpeciesNxDataset, SpeciesDGLDataset, FlexibleSpeciesDGLDataset
-from .lahelr import lahelr_print
+from .lahelr import lahelr_print,lahelr_time
