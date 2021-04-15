48954dc13ec233970e5d434129be05bdaf90bcf2
LahElr <136647680@qq.com>
2021-04-15 15:53:15 +0800

    Update lahelr.py

--- utils/lahelr.py
+++ utils/lahelr.py
@@ -1,4 +1,9 @@
+import time
+
 def lahelr_print(*args,sep=None,end=None):
     pass
-    # print(*args,sep=sep,end=end)
+    print(*args,sep=sep,end=end)
     # print()
+
+def lahelr_time():
+    return time.asctime(time.localtime(time.time()))
